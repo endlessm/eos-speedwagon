9aef7f70d36bba729b36c5d3b3d07a0af558a48e
Jasper St. Pierre <jstpierre@mecheye.net>
2015-01-21 13:01:44 -0800

    Use a standard spinner
    Rather than our own custom library

--- Makefile
+++ Makefile
@@ -1,31 +1,7 @@
 
-MKDIR_P = mkdir -p
-
 CLEANFILES =
-PKGS = gtk+-3.0
-CFLAGS = $(shell pkg-config --cflags $(PKGS)) -Wall -Werror
-LDFLAGS = $(shell pkg-config --libs $(PKGS))
 
-FILES = src/lib/speedwagon-spinner.c src/lib/speedwagon-spinner.h
-
-all: libspeedwagon.so Speedwagon-1.0.typelib speedwagon.gresource
-
-INTROSPECTION_GIRS = Speedwagon-1.0.gir
-INTROSPECTION_SCANNER_ARGS = --warn-all --warn-error --no-libtool
-
-Speedwagon-1.0.gir: libspeedwagon.so
-Speedwagon_1_0_gir_INCLUDES = Gtk-3.0
-Speedwagon_1_0_gir_CFLAGS = $(CFLAGS)
-Speedwagon_1_0_gir_LIBS = speedwagon
-Speedwagon_1_0_gir_FILES = $(FILES)
-CLEANFILES += Speedwagon-1.0.gir Speedwagon-1.0.typelib
-
-include Makefile.introspection
-
-libspeedwagon.so: CFLAGS += -fPIC -shared
-libspeedwagon.so: src/lib/speedwagon-spinner.o
-	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
-CLEANFILES += libspeedwagon.so speedwagon-spinner.o
+all: speedwagon.gresource
 
 resource_files = $(shell glib-compile-resources --generate-dependencies speedwagon.gresource.xml)
 speedwagon.gresource: speedwagon.gresource.xml $(resource_files)
--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@ const Gdk = imports.gi.Gdk;
 const GdkX11 = imports.gi.GdkX11;
 
 const Gtk = imports.gi.Gtk;
-const Speedwagon = imports.gi.Speedwagon;
 
 const Format = imports.format;
 const Lang = imports.lang;
@@ -29,82 +28,12 @@ const SpeedwagonIface = '<node> \
 
 const SPLASH_CIRCLE_PERIOD = 2;
 const SPLASH_SCREEN_FADE_OUT = 1;
+
+// This needs to match the time of the transitions in speedwagon.css
 const SPLASH_SCREEN_COMPLETE_TIME = 1;
 
 const SPLASH_BACKGROUND_DESKTOP_KEY = 'X-Endless-SplashBackground';
 const DEFAULT_SPLASH_SCREEN_BACKGROUND = 'resource:///com/endlessm/Speedwagon/splash-background-default.jpg';
-
-const Spinner = new Lang.Class({
-    Name: 'EosSpeedwagonSpinner',
-    Extends: Speedwagon.Spinner,
-
-    _init: function(params) {
-        this.parent(params);
-
-        this.get_style_context().add_class('speedwagon-spinner');
-
-        this._spinner = new Gtk.Image({ visible: true, resource: '/com/endlessm/Speedwagon/splash-spinner.png' });
-        this._spinner.set_parent(this);
-        this._glowChannel = new Gtk.Image({ visible: true, resource: '/com/endlessm/Speedwagon/splash-spinner-channel-glow.png' });
-        this._glowChannel.set_parent(this);
-        this._glowSpinner = new Gtk.Image({ visible: true, resource: '/com/endlessm/Speedwagon/splash-spinner-glow.png' });
-        this._glowSpinner.set_parent(this);
-        this.set_internal_widgets([this._spinner, this._glowChannel, this._glowSpinner]);
-
-        this._rotation = 0;
-        this.glowOpacity = 0;
-    },
-
-    set glowOpacity(value) {
-        this._glowOpacity = value;
-        this._glowChannel.opacity = value;
-        this._glowSpinner.opacity = value;
-    },
-    get glowOpacity() {
-        return this._glowOpacity;
-    },
-
-    vfunc_size_allocate: function(rect) {
-        this.parent(rect);
-        this._spinner.size_allocate(rect);
-        this._glowChannel.size_allocate(rect);
-        this._glowSpinner.size_allocate(rect);
-    },
-
-    vfunc_draw: function(cr) {
-        // Draw the background -- the standard channel
-        let context = this.get_style_context();
-        let alloc = this.get_allocation();
-        Gtk.render_background(this.get_style_context(), cr, 0, 0, alloc.width, alloc.height);
-
-        // Draw all the normal elements
-        this.foreach(Lang.bind(this, function(child) {
-            this.propagate_draw(child, cr);
-        }));
-
-        // Draw the channel glow
-        this.propagate_draw(this._glowChannel, cr);
-
-        // Draw the spinner
-        cr.translate(alloc.width / 2, alloc.height / 2);
-        cr.rotate(this._rotation * Math.PI * 2);
-        cr.translate(-alloc.width / 2, -alloc.height / 2);
-        this.propagate_draw(this._glowSpinner, cr);
-        this.propagate_draw(this._spinner, cr);
-
-        cr.$dispose();
-    },
-
-    tick: function(clock) {
-        let time = clock.get_frame_time() / GLib.USEC_PER_SEC;
-        if (!this._startTime)
-            this._startTime = time;
-
-        // 1s = 1 full turn of the spinner
-        this._rotation = (time - this._startTime) / SPLASH_CIRCLE_PERIOD;
-        this.queue_draw();
-    },
-});
 
 const Window = new Lang.Class({
     Name: 'EosSpeedwagonWindow',
@@ -124,25 +53,29 @@ const Window = new Lang.Class({
         gdkWindow.set_utf8_property('_GTK_APPLICATION_ID', appID);
 
         this._buildUI();
-
-        this._rampOutStartTime = 0;
-
-        // Start the spinning...
-        this.add_tick_callback(Lang.bind(this, this._tick));
     },
 
     _buildUI: function() {
-        let baseBox = new Gtk.Box({ visible: true });
-        let context = baseBox.get_style_context();
+        this._baseBox = new Gtk.Box({ visible: true });
+        let context = this._baseBox.get_style_context();
         context.add_class('speedwagon-bg');
 
-        this._spinner = new Spinner({ visible: true,
-                                      width_request: 220,
-                                      height_request: 220,
-                                      hexpand: true, vexpand: true,
-                                      halign: Gtk.Align.CENTER,
-                                      valign: Gtk.Align.CENTER });
-        baseBox.add(this._spinner);
+        this._spinner = new Gtk.Spinner({ visible: true,
+                                          width_request: 220,
+                                          height_request: 220,
+                                          hexpand: true, vexpand: true,
+                                          halign: Gtk.Align.CENTER,
+                                          valign: Gtk.Align.CENTER });
+        this._spinner.get_style_context().add_class('speedwagon-spinner');
+        this._spinner.start();
+
+        let spinnerBox = new Gtk.Box({ visible: true });
+        spinnerBox.get_style_context().add_class('speedwagon-spinner-bg');
+
+        let overlay = new Gtk.Overlay({ visible: true });
+        overlay.add(spinnerBox);
+        overlay.add_overlay(this._spinner);
+        this._baseBox.add(overlay);
 
         let gicon = this._appInfo.get_icon();
         let image = new Gtk.Image({ visible: true,
@@ -151,7 +84,7 @@ const Window = new Lang.Class({
                                     valign: Gtk.Align.CENTER,
                                     pixel_size: 64,
                                     gicon: gicon });
-        this._spinner.add(image);
+        spinnerBox.add(image);
 
         let bgPath;
         if (this._appInfo.has_key(SPLASH_BACKGROUND_DESKTOP_KEY)) {
@@ -164,31 +97,31 @@ const Window = new Lang.Class({
         provider.load_from_data(Format.vprintf('.speedwagon-bg { background-image: url("%s"); }', [bgPath]));
         context.add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
 
-        this.add(baseBox);
+        this.add(this._baseBox);
     },
 
-    _tick: function(window, clock) {
-        this._spinner.tick(clock);
-
-        if (this._rampOutStartTime) {
-            let time = clock.get_frame_time();
-            let dt = (time - this._rampOutStartTime) / GLib.USEC_PER_SEC;
-
-            if (dt < SPLASH_SCREEN_COMPLETE_TIME) {
-                this._spinner.glowOpacity = dt / SPLASH_SCREEN_COMPLETE_TIME;
-            } else if (dt - SPLASH_SCREEN_COMPLETE_TIME < SPLASH_SCREEN_FADE_OUT) {
-                this.opacity = 1.0 - (dt - SPLASH_SCREEN_COMPLETE_TIME) / SPLASH_SCREEN_FADE_OUT;
-            } else {
-                this.destroy();
-            }
-        }
-
+    _fadeOutTick: function(window, clock) {
+        let time = clock.get_frame_time();
+        let dt = (time - this._fadeOutStartTime) / GLib.USEC_PER_SEC;
+        let opacity = 1.0 - (dt / SPLASH_SCREEN_FADE_OUT);
+        if (opacity > 0)
+            this.opacity = opacity;
+        else
+            this.destroy();
         return true;
+    },
+    _fadeOut: function() {
+        this._fadeOutStartTime = this.get_frame_clock().get_frame_time();
+        this.add_tick_callback(Lang.bind(this, this._fadeOutTick));
     },
 
     rampOut: function() {
-        let clock = this.get_frame_clock();
-        this._rampOutStartTime = clock.get_frame_time();
+        this._baseBox.get_style_context().add_class('glow');
+
+        GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, SPLASH_SCREEN_COMPLETE_TIME, Lang.bind(this, function() {
+            this._fadeOut();
+            return false;
+        }));
     },
 });
 
